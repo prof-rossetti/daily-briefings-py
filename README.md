a5edb5c5ccc8eff5eda011aeef6f4ffb93cc9451
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-28 20:50:10 -0400

    Stocks service

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # daily-briefings-py
+
+## Setup
+
+Fork this repo and clone it onto your local computer (for example to your Desktop), then navigate there from the command-line:
+
+```sh
+cd ~/Desktop/daily-briefings-py/
+```
+
+Create and activate a new Anaconda virtual environment, perhaps named "briefings-env":
+
+```sh
+conda create -n briefings-env python=3.7
+conda activate briefings-env
+```
+
+Then, from within the virtual environment, install package dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+From within the virtual environment, ensure you can run each of the files and see them produce their desired results of: printing the latest closing prices, printing today's weather forecast, and sending an example email, respectively.
+
+```sh
+python app/stocks_service.py
+#> THE LATEST CLOSING PRICE IS ...
+```
+
+```sh
+python app/weather_service.py
+#> TODAY'S WEATHER FORECAST IS ...
+```
+
+```sh
+python app/email_service.py
+#> SENDING EMAIL TO ...
+```
+
+> NOTE: the Sendgrid emails might first start showing up in spam, until you designate them as coming from a trusted source
+
+Also demonstrate your ability to send the daily briefing email:
+
+```sh
+python app/daily_briefing.py
+```
--- app/stocks_service.py
+++ app/stocks_service.py
@@ -1,3 +1,41 @@
 # app/stocks_service.py
 
-# TODO
+import os
+import json
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="abc123")
+MY_STOCKS = os.getenv("MY_STOCKS", default="MSFT,GOOG,TSLA")
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}"  #> $12,000.71
+
+symbols = MY_STOCKS.split(",") #> ["MSFT", "GOOG", "TSLA"]
+
+for symbol in symbols:
+    print("---------------------")
+    print("SYMBOL:", symbol)
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    if "Time Series (Daily)" in parsed_response.keys():
+        tsd = parsed_response["Time Series (Daily)"]
+        latest_date, latest_prices = list(tsd.items())[0]
+        latest_close = latest_prices["4. close"]
+        print("LATEST DAY:", latest_date)
+        print("LATEST CLOSE:", to_usd(float(latest_close)))
+    else:
+        print(parsed_response)
