439958d63a4719656ad5410b42ec9a206b6ddb12
jenniferlu123 <59629822+jenniferlu123@users.noreply.github.com>
2020-04-06 22:27:08 -0400

    Added basic layout

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,23 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint, render_template
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    print("VISITED THE HOME PAGE")
+    return render_template("home.html")
+    #return "Welcome Home (TODO)"
+
+@home_routes.route("/about")
+def about():
+    print("VISITED THE ABOUT PAGE")
+    return render_template("about.html")
+    #return "About Me (TODO)"
+
+@home_routes.route("/register")
+def register():
+    print("VISITED THE REGISTRATION PAGE")
+    return render_template("home.html")
+    #return "Sign Up for our Product! (TODO)"
