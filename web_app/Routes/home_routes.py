950a7926351b3710167050e8d0d2e5d86487dda6
coder489 <aai34@georgetown.edu>
2020-04-08 14:37:21 -0700

    Created the templates

--- web_app/Routes/home_routes.py
+++ web_app/Routes/home_routes.py
@@ -7,15 +7,16 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/")
 def index():
     print("VISITED THE HOME PAGE")
-    #return render_template("dashboard.html")
-    return "Welcome Home (TODO)"
+    #return "Welcome Home (TODO)"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
-    return "About Me (TODO)"
+    #return "About Me (TODO)"
+    return render_template("about.html")
 
 @home_routes.route("/register")
 def register():
     print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)"
+    return "Sign Up for our Product! (TODO)" # we'll make an HTML template for this later!
