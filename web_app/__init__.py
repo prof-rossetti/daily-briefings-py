20674973ad9e614356be7c6f0871cf47c12318db
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-04-08 18:07:16 -0400

    add weather forecast nav

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.weather_routes import weather_routes
+from web_app.routes.weather_routes import weather_routes
 
 load_dotenv()
 
@@ -17,7 +17,7 @@ def create_app():
     app.config["SECRET_KEY"] = SECRET_KEY
 
     app.register_blueprint(home_routes)
-    #app.register_blueprint(weather_routes)
+    app.register_blueprint(weather_routes)
 
     return app
 
