e19721af127352135382afbf293b7a2f53f0efdd
MJ Rossetti <s2t2@users.noreply.github.com>
2021-03-24 12:06:41 -0400

    Now we don't need to repeat the formatting logic

--- app/daily_briefing.py
+++ app/daily_briefing.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from datetime import date
 
 from app import APP_ENV
-from app.weather_service import get_hourly_forecasts, set_geography, format_hour, format_temp
+from app.weather_service import get_hourly_forecasts, set_geography
 from app.email_service import send_email
 
 load_dotenv()
@@ -25,14 +25,12 @@ if __name__ == "__main__":
 
     # FETCH DATA
 
-    geo, result = get_hourly_forecasts(country_code=user_country, zip_code=user_zip)
-    if not (geo.any() and result):
+    result = get_hourly_forecasts(country_code=user_country, zip_code=user_zip)
+    if not result:
         print("INVALID GEOGRAPHY. PLEASE CHECK YOUR INPUTS AND TRY AGAIN!")
         exit()
 
     # DISPLAY OUTPUTS
-
-    city_name = f"{geo.place_name}, {geo.state_code}"
 
     html = ""
     html += f"<h3>Good Morning, {USER_NAME}!</h3>"
@@ -40,12 +38,10 @@ if __name__ == "__main__":
     html += "<h4>Today's Date</h4>"
     html += f"<p>{date.today().strftime('%A, %B %d, %Y')}</p>"
 
-    html += f"<h4>Weather Forecast for {city_name}</h4>"
+    html += f"<h4>Weather Forecast for {result['city_name']}</h4>"
     html += "<ul>"
-    for forecast in result["properties"]["periods"][0:24]:
-        hour = format_hour(forecast['startTime'])
-        temp = format_temp(forecast['temperature'])
-        html += f"<li>{hour} | {temp} | {forecast['shortForecast'].upper()}</li>"
+    for forecast in result["hourly_forecasts"]:
+        html += f"<li>{forecast['timestamp']} | {forecast['temp']} | {forecast['conditions'].upper()}</li>"
     html += "</ul>"
 
     send_email(subject="[Daily Briefing] My Morning Report", html=html)
