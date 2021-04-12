615f79cfc111155003ccea3f80702b22de0c6d15
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-12 18:19:01 -0400

    Render HTML template

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, request #, render_template
+from flask import Blueprint, request, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -8,12 +8,14 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
+    #return "Welcome Home"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
+    #return "About Me"
+    return render_template("about.html")
 
 @home_routes.route("/hello")
 def hello_world():
@@ -22,4 +24,6 @@ def hello_world():
     # ... which will return None instead of throwing an error if key is not present
     # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
     name = request.args.get("name") or "World"
-    return f"Hello, {name}!"
+    #return f"Hello, {name}!"
+    message = f"Hello, {name}!"
+    return render_template("hello.html", message=message)
