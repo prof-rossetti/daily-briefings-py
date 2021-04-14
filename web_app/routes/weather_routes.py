be4b9a3f385cbf9c316c24381a7e3fdcc8b16d3a
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-14 18:02:37 -0400

    Flash messaging

--- web_app/routes/weather_routes.py
+++ web_app/routes/weather_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/weather_routes.py
 
-from flask import Blueprint, request, jsonify, render_template, redirect
+from flask import Blueprint, request, jsonify, render_template, redirect, flash
 
 from app.weather_service import get_hourly_forecasts
 
@@ -42,8 +42,8 @@ def weather_forecast():
 
     results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
     if results:
-        #flash(f"Weather Forecast Generated Successfully!", "success")
+        flash(f"Weather Forecast Generated Successfully!", "success")
         return render_template("weather_forecast.html", country_code=country_code, zip_code=zip_code, results=results)
     else:
-        #flash(f"Geography Error. Please try again!", "danger")
+        flash(f"Geography Error. Please try again!", "danger")
         return redirect("/weather/form")
